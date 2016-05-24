a2205bc7d726917a078722ecde8fa1c02ab4067e
AG8 <andrew2000g@gmail.com>
2016-05-23 20:53:54 -0400

    Another commit to look cool

--- src/org/ag8/itoocanbecool/Main.java
+++ src/org/ag8/itoocanbecool/Main.java
@@ -1,8 +1,10 @@
 package org.ag8.itoocanbecool;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /*
 git add .
@@ -11,9 +13,13 @@ git push
  */
 
 public class Main {
+    static Random rand = new Random();
+
     public static void main(String[] args) throws IOException {
+        makeFileCooler();
+
         ProcessBuilder builder = new ProcessBuilder(
-                "cmd.exe", "/c", "cd \"C:\\Users\\Andrew\\workspace\\f\\itoocanbecool\" && git status");
+                "cmd.exe", "/c", "cd \"C:\\Users\\Andrew\\workspace\\f\\itoocanbecool\" && git add . && git commit -m \"Another commit to look cool\" && git push");
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -25,5 +31,23 @@ public class Main {
             }
             System.out.println(line);
         }
+    }
+
+    private static void makeFileCooler() throws IOException {
+        // Decide whether to append or start over
+        int k = randInt(0, 1000);
+
+        if (k == 1) {
+            PrintWriter writer = new PrintWriter("cool.txt");
+            writer.print("");
+            writer.close();
+        } else {
+            FileUtils.writeStringToFile(new File("cool.txt"), "a", true);
+        }
+    }
+
+    // http://stackoverflow.com/a/363692/2930268
+    public static int randInt(int min, int max) {
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
