4cc197ebb8c3400e54517be518828af25565f0f2
AG8 <andrew2000g@gmail.com>
2016-05-23 20:45:20 -0400

    Another commit

---
+++ src/org/ag8/itoocanbecool/Main.java
@@ -0,0 +1,23 @@
+package org.ag8.itoocanbecool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/c", "cd \"C:\\Users\\Andrew\\workspace\\f\\itoocanbecool\" && git status");
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            System.out.println(line);
+        }
+    }
+}
